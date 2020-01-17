5b1eff86164c2034267b6c3287efd47339cde897
Viktor Andersson <viktor.nossredna@gmail.com>
2020-01-17 17:17:34 +0100

    Put output strings within quotes

--- install.ps1
+++ install.ps1
@@ -1,21 +1,21 @@
-Write-Output ============== Environment Installation Script ==============
+Write-Output "============== Environment Installation Script =============="
 Write-Output .
-Write-Output Browse https://chocolatey.org/packages for packages
+Write-Output "Browse https://chocolatey.org/packages for packages"
 Write-Output .
-Write-Output Ensure that your cmd.exe runs as Administrator
-Write-Output Do not forget to check proxy settings
+Write-Output "Ensure that your cmd.exe runs as Administrator"
+Write-Output "Do not forget to check proxy settings"
 Write-Output .
 pause
 
-Write-Output Install Chocolaty
+Write-Output "Install Chocolaty"
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature enable -n=allowGlobalConfirmation
 Write-Output .
-Write-Output Chocolatey installed
+Write-Output "Chocolatey installed"
 Write-Output .
 
 choco install autoruns
-Write-Output autoruns installed
+Write-Output "autoruns installed"
 
 pause
